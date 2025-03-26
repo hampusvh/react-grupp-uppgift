ebd42c95623f972ed3e500eae3934bbf7f01fe23
hampusvh <hampus.vh@gmail.com>
2025-03-26 10:04:40 +0100

    auction-cards styling

--- auktions-sida/frontend/src/components/AuctionList.jsx
+++ auktions-sida/frontend/src/components/AuctionList.jsx
@@ -10,32 +10,26 @@ const AuctionList = ({ auctions }) => {
 
   return (
     <div>
-      <h2>Auktioner</h2>
+      <h2 className="auctions-title">Pågående auktioner</h2>
       <div className="auction-list">
         {auctions.map((auction) => (
           <div key={auction._id} className="auction-card">
             <div className="image-placeholder">Bild</div>
             <p>
-              <strong>{auction.title}</strong>
+              <strong className="card-title">{auction.title}</strong>
             </p>
-            <p>Startpris: {auction.price} kr</p>
-            {auction.description && <p>Beskrivning: {auction.description}</p>}
-            {auction.startDate && (
+            <p>
+              <strong>Startpris:</strong> {auction.price} kr
+            </p>
+            {auction.description && (
               <p>
-                Start:{" "}
-                {new Date(auction.startDate).toLocaleString("sv-SE", {
-                  weekday: "long",
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
+                <strong>Beskrivning:</strong> {auction.description}
               </p>
             )}
+
             {auction.endDate && (
               <p>
-                Slut:{" "}
+                <strong>Slut:</strong>{" "}
                 {new Date(auction.endDate).toLocaleString("sv-SE", {
                   weekday: "long",
                   year: "numeric",
@@ -46,7 +40,11 @@ const AuctionList = ({ auctions }) => {
                 })}
               </p>
             )}
-            {auction.createdBy && <p>Skapad av: {auction.createdBy}</p>}
+            {auction.createdBy && (
+              <p>
+                <strong>Skapad av:</strong> {auction.createdBy}
+              </p>
+            )}
             <button className="bid-btn">Lägg bud nu!</button>
           </div>
         ))}
